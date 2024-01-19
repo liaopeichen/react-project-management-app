584df890c4462763b6f12ce56d72bec9cd8f5253
liaopeichen <152478331+liaopeichen@users.noreply.github.com>
2024-01-19 17:00:37 +0800

    apply styles to the buttons in NewProject and Inputs with Tailwind CSS

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,20 @@
 export default function Input({ label, textarea, ...props }) {
+  const classes =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+
   return (
-    <p>
-      <label htmlFor={label}>{label}</label>
-      <input
-        type={textarea ? <textarea {...props} /> : <input {...props} />}
-        id={label}
-      />
+    <p className="flex flex-col gap-1 my-4">
+      <label
+        htmlFor={label}
+        className="text-sm font-bold uppercase text-stone-500"
+      >
+        {label}
+      </label>
+      {textarea ? (
+        <textarea {...props} id={label} className={classes} />
+      ) : (
+        <input {...props} id={label} className={classes} />
+      )}
     </p>
   );
 }
